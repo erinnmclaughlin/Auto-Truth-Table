fdcb36fae296723e7ac30a1f24f707757c13060a
Erin McLaughlin <emclaughlin@lighthouseinstruments.com>
2019-07-02 10:49:20 -0400

    Added parenthesis support (draft)

--- LogicalEquiv.Domain/TruthTable.cs
+++ LogicalEquiv.Domain/TruthTable.cs
@@ -8,18 +8,64 @@ namespace LogicalEquiv.Domain
 {
     public class TruthTable
     {
-        public static void Write(string statement)
-        {
-            List<Proposition> Propositions = Proposition.GetPropositions(statement);
+        public string Statement { get; set; }
+        public List<Proposition> Propositions { get; set; }
+        public List<TruthTableRow> Rows { get; set; }
 
+        private string SimplifiedStatement { get; set; }
+
+        public TruthTable(string statement)
+        {
+            Statement = statement;
+            SimplifiedStatement = statement;
+            Propositions = new List<Proposition>();
+            Rows = new List<TruthTableRow>();
+            InitializePropositions();
+            Write();
+        }
+
+        private void InitializePropositions()
+        {
+            Propositions = new List<Proposition>();
+
+            //-- First find all individual propositions -- set them all to false
+            //-- Make sure that props aren't double counted
+            foreach (char c in Statement)
+            {
+                if (c >= 'a' && c <= 'z' && Propositions.Where(p => p.Name == c.ToString()).Count() == 0)
+                    Propositions.Add(new Proposition(c.ToString(), false));
+            }
+        }
+
+        private List<Proposition> FilterPropositions(string statement, List<Proposition> p)
+        {
+            List<Proposition> props = new List<Proposition>();
+
+            foreach (var prop in Propositions)
+            {
+                if (statement.Contains(prop.Name) && !p.Contains(prop))
+                    props.Add(prop);
+            }
+
+            foreach(var prop in p)
+            {
+                if (statement.Contains(prop.Name) && prop.Name.Length == 1)
+                    props.Add(prop);
+            }
+
+            return props;
+        }
+
+        public void Write()
+        {
             foreach (var prop in Propositions)
             {
                 Console.Write($"{prop.Name}\t");
             }
 
-            Console.Write($"{statement}\n");
+            Console.Write($"{Statement}\n");
 
-            statement = statement.Replace(" ", "");
+            Statement = Statement.Replace(" ", "");
 
             // Loop through all possible scenarios, creating 2^n rows
             for (int i = 0; i < Math.Pow(2, Propositions.Count()); i++)
@@ -40,34 +86,38 @@ namespace LogicalEquiv.Domain
                     Console.Write($"{prop.Value}\t");
                 }
 
-                // Create a copy of Propositions to modify while determining truth value of statement
-                List<Proposition> Temp = new List<Proposition>();
-                Propositions.ForEach(p => Temp.Add(p));
-
                 // Determine the value of the statement given the values of the propositions
-                while(statement.Contains("("))
+                string tempStatement = Statement;
+                List<Proposition> tempPropositions = Propositions;
+
+                char newProp = 'A';
+                while (tempStatement.Contains("("))
                 {
                     // Find innermost "(" ")" pair
                     int sIndex = -1, eIndex = -1;
-                    for (int j = 0; j < statement.Length; j++)
+                    for (int j = 0; j < tempStatement.Length; j++)
                     {
-                        if (statement[j] == '(')
+                        if (tempStatement[j] == '(')
                             sIndex = j;
-                        else if (statement[j] == ')')
+                        else if (tempStatement[j] == ')')
                         {
                             eIndex = j;
-                            continue;
+                            break;
                         }
                     }
 
                     // Compute what's inside
-                    string substring = statement.Substring(sIndex + 1, eIndex - sIndex - 1);
-                    Console.WriteLine(substring);
-                    Console.ReadKey();
-                    
+                    // Something here isn't working bc passing too many props for a given statement, fix later
+                    string substring = tempStatement.Substring(sIndex + 1, eIndex - sIndex - 1);
+                    tempPropositions = FilterPropositions(substring, tempPropositions);
+
+                    tempPropositions.Add(new Proposition(newProp.ToString(), Logic.Compute(substring, tempPropositions)));
+                    tempStatement = tempStatement.Replace("(" + substring + ")", tempPropositions.Last().Name);
+                    tempPropositions = FilterPropositions(tempStatement, tempPropositions);
+                    newProp++;
                 }
 
-                Console.Write(Temp.Last().Value);
+                Console.Write(Logic.Compute(tempStatement, tempPropositions));
                 Console.WriteLine();
             }
         }
